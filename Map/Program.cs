68bf6e8daa004f5f2c5bd881e282daa2db0009d9
Lethme <lethme@ya.ru>
2020-12-09 00:33:17 +0400

    Initial commit

--- Map/Program.cs
+++ Map/Program.cs
@@ -6,31 +6,7 @@ namespace Map
     {
         static void Main(string[] args)
         {
-            var map = Map.Create
-            (
-                MapItem.Create(
-                    5,
-                    "Nico",
-                    "Ded",
-                    "Shvabra"
-                ),
-                MapItem.Create(
-                    3,
-                    "dfgjkd",
-                    "dkdjghgfh",
-                    "Nico"
-                )
-            );
-
-            Console.WriteLine($"\n{map}\n");
-
-            map.SortItems(SortType.Ascending);
-
-            Console.WriteLine($"\n{map}\n");
-
-            map.SortData(SortType.Descending);
-
-            Console.WriteLine($"\n{map}\n");
+            
         }
     }
 }
