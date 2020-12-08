db04b9be5d647fedb230039163d687bfc94bd0ce
Пожилой шмель <63866529+Lethme@users.noreply.github.com>
2020-12-09 00:30:45 +0400

    Initial commit

---
+++ Map/MapExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Map
+{
+    public static class MapExtensions
+    {
+        public static string AsIndentedString<TKey, TData>(this Map<TKey, TData> source, String separator = "\n") where TKey : IComparable<TKey>, IEquatable<TKey> where TData : IComparable<TData>, IEquatable<TData>
+        {
+            return source.Select(item => item.ToString()).Aggregate((x, y) => $"{x}{separator}{y}");
+        }
+    }
+}
---
+++ Map/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Map
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = Map.Create
+            (
+                MapItem.Create(
+                    5,
+                    "Nico",
+                    "Ded",
+                    "Shvabra"
+                ),
+                MapItem.Create(
+                    3,
+                    "dfgjkd",
+                    "dkdjghgfh",
+                    "Nico"
+                )
+            );
+
+            Console.WriteLine($"\n{map}\n");
+
+            map.SortItems(SortType.Ascending);
+
+            Console.WriteLine($"\n{map}\n");
+
+            map.SortData(SortType.Descending);
+
+            Console.WriteLine($"\n{map}\n");
+        }
+    }
+}
