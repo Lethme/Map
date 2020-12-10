4940e6f33900ebb430abfa379c07fe2a85c63b91
Lethme <lethme@ya.ru>
2020-12-10 11:49:40 +0400

    Hash class added

---
+++ Map/BinaryTree/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Map.BinaryTree
+{
+    public class BinaryTree<T> : IEnumerable<T>
+    {
+        private List<T> Items { get; } = new List<T>();
+        public int Count => Items.Count;
+        public bool Empty => Items.Count == 0;
+        public BinaryTree() {  }
+        public T this[int index] => Items[index];
+        public void Add(T obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            Items.Add(obj);
+        }
+        public T GetLeftChildByIndex(int index)
+        {
+            if (Items.Count >= (2 * index + 1))
+                return Items[2 * index + 1];
+            return default;
+        }
+        public T GetRightChildByIndex(int index)
+        {
+            if (Items.Count >= (2 * index + 2))
+                return Items[2 * index + 2];
+            return default;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+    }
+}
---
+++ Map/Hash/Hash.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Map.Hash
+{
+#nullable enable
+    public class Hash : IEquatable<Hash>
+    {
+        public String Data { get; private set; } = String.Empty;
+        public Hash() {  }
+        public Hash(object? obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            this.Data = Encrypt(obj);
+        }
+        public void UpdateHash(object? obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            this.Data = Encrypt(obj);
+        }
+        public override bool Equals(object? obj)
+        {
+            return this.Data.Equals(Encrypt(obj));
+        }
+        public bool Equals(Hash? other)
+        {
+            return this.Data.Equals(other.Data);
+        }
+        public override string ToString() => this.Data;
+        public static bool operator ==(Hash hash, object obj) => hash.Equals(obj);
+        public static bool operator ==(Hash left, Hash right) => left.Equals(right);
+        public static bool operator !=(Hash hash, object obj) => !hash.Equals(obj);
+        public static bool operator !=(Hash left, Hash right) => !left.Equals(right);
+        public override int GetHashCode() => this.Data.GetHashCode();
+        public static string Encrypt(object? obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            using var sha256 = SHA256.Create();
+            var result = sha256.ComputeHash(Encoding.Default.GetBytes(JsonConvert.SerializeObject(obj)));
+            return BitConverter.ToString(result).Replace("-", String.Empty).ToLower();
+        }
+        public static Hash Create() => new Hash();
+        public static Hash Create(object? obj) => new Hash(obj);
+    }
+#nullable disable
+}
